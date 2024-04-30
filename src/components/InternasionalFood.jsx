891aa08dd13b4faf86d7291a9e084189e6db351a
Gesit Widi Atmoko <140772511+gesitwidyatsmo@users.noreply.github.com>
2024-04-30 21:15:04 +0700

    Fix: image Internasional Food

--- src/components/InternasionalFood.jsx
+++ src/components/InternasionalFood.jsx
@@ -64,7 +64,7 @@ const InternasionalFood = () => {
           {negara.slice(0, 10).map((negara, i) => (
             <SwiperSlide key={i} className='relative'>
               <Link href={`/masakan-internasional/${negara.strArea}`}>
-                <img src={negara.imageUrl} alt={negara.strIngredient} className='flex justify-center items-center rounded-xl mx-auto h-32 md:h-40 lg:h-44 w-full  brightness-50' />
+                <img src={`/${negara.imageUrl}`} alt={negara.strIngredient} className='flex justify-center items-center rounded-xl mx-auto h-32 md:h-40 lg:h-44 w-full  brightness-50' />
                 <div className='absolute bottom-[50%] translate-y-[50%] w-full text-center bg-opacity-75'>
                   <h3 className='text-xl font-bold text-white py-2'>{negara.strArea}</h3>
                 </div>
